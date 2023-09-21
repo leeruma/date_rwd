f6fbbb5619fd5a666875b202d24dc7218b955017
leeruma <achieve4525@naver.com>
2023-09-21 11:19:04 +0900

    시간별 배경색 변경

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ setInterval(반복실행할 함수, 반복시간간격시간(ms))
 const [hr, m, s] = document.querySelectorAll('h1 span');
 setInterval(setTime, 1000);
 
+// 함수 추가
+// 원하는 시간마다 body에 morning, afternoon, evening, night
+function changeClass(hr) {
+	if (hr >= 6 && hr < 12) document.body.classList.add('morning');
+	if (hr >= 12 && hr < 16) document.body.classList.add('afternoon');
+	if (hr >= 16 && hr < 20) document.body.classList.add('evening');
+	if (hr >= 20 || hr < 6) document.body.classList.add('night');
+}
+
 function setTime() {
 	const now = new Date();
 	const hours = now.getHours();
@@ -13,6 +22,8 @@ function setTime() {
 	hr.innerText = setNumbers(setHours(hours));
 	m.innerText = setNumbers(min);
 	s.innerText = setNumbers(sec);
+
+	changeClass(hours);
 }
 
 function setNumbers(num) {
