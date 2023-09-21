7985ddec2eb6d352c4204ed17b0158f35caf0988
leeruma <achieve4525@naver.com>
2023-09-21 10:25:38 +0900

    시분초 출력

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
+const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
 
-console.log(setNumbers(hours));
-
-function setNumbers(num) {
-	/*
-	if (num < 10) result = '0' + num;
-	else result = num;
-  */
-	// 조건식 ? true일때 실행할 구문 : false일때 실행할 구문
-	// 삼항연산자 구문안에는 대입연산자가 들어올 수 없기 떄문에 괄호로 묶어서 표현식으로 변경
-	// 특정값이 else 일때 굳이 실행할 코드가 없다면 &&연산자 활용
-	// num < 10 ? (result = '0' + num) : (result = num);
-	return num < 10 && (num = '0' + num);
-}
+// hours값을 setHours가 조건에 12를 뺀 값으로 반환
+// 반환된 결과값을 다시 바로 setNumbers에 집어넣어서 0을 붙이도록 처리
+hr.innerText = setNumbers(setHours(hours));
+m.innerText = setNumbers(min);
+s.innerText = setNumbers(sec);
 
 /*
-let newHr = 0;
-
-if (hours > 12) {
-	newHr = hours - 12;
-	if (newHr < 10) {
-		newHr = '0' + newHr + 'PM';
-	} else {
-		newHr = newHr + 'PM';
-	}
-} else {
-	newHr = hours;
-	if (newHr < 10) {
-		newHr = '0' + newHr + 'AM';
-	} else {
-		newHr = newHr + 'AM';
-	}
-}
+// 다음과 같이 코드를 줄일 수 있음
+const values = [setHours(hours), min, sec];
+spans.forEach((el, idx) => {
+	el.innerText = setNumbers(values[idx]);
+});
 */
+
+function setNumbers(num) {
+	num < 10 && (num = '0' + num);
+	return num;
+}
+
+function setHours(num) {
+	num > 12 && (num = num - 12);
+	return num;
+}
